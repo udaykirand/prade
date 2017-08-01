412d9b452342d9310c7cbe4f13077792412d036a
udadh <udaykiran.dharbamulla@idainfront.se>
2017-08-01 22:37:18 +0530

    Redirecting to home on product creation

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -85,7 +85,7 @@ export class ProductComponent implements OnInit {
     this.productService.createProduct(this.form.value)
     .subscribe(data => {
       console.log(data.data);
-      this.successMessage = 'Product created successfully. Product Id: '+data.data;
+      this.router.navigate('/'');
     },
     error => {
       console.log(error.status);
